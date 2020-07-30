ae41e5df9838850c4e8d6f4ff7148a87f2db7663
Byt3ChkR <ti.akermann@gmail.com>
2020-07-30 19:33:55 +0200

    Converted the Framework to .NetStandard and added Unity Reference as External Library

---
+++ VDFramework.Standard/StandardFramework/Singleton/Singleton.cs
@@ -0,0 +1,61 @@
+﻿namespace VDFramework.Singleton
+{
+	using Exceptions;
+
+	/// <summary>
+	/// A abstract generic implementation of the singleton pattern
+	/// </summary>
+	/// <typeparam name="T">The type to create a singleton of</typeparam>
+	public abstract class Singleton<T>
+		where T : Singleton<T>, new()
+	{
+		private static T instance;
+
+		public static T Instance
+		{
+			get
+			{
+				// ReSharper disable once ConvertIfStatementToNullCoalescingExpression
+				if (instance == null)
+				{
+					instance = SingletonInstanceCreator<T>.CreateInstance();
+				}
+
+				return instance;
+			}
+			set => instance = value;
+		}
+
+		public static T InstanceIfInitialized => IsInitialized ? instance : null;
+
+		public static bool IsInitialized => instance != null;
+
+		protected Singleton()
+		{
+			if (!IsInitialized)
+			{
+				Instance = this as T;
+			}
+			else
+			{
+				throw new SingletonViolationException();
+			}
+		}
+
+		~Singleton()
+		{
+			if (instance == this)
+			{
+				instance = null;
+			}
+		}
+		
+		/// <summary>
+		/// Sets the instance of the singleton to null.
+		/// </summary>
+		public void DestroyInstance()
+		{
+			instance = null;
+		}
+	}
+}
---
+++ VDUnityFramework.Standard/UnityExtensions/GameObjectExtensions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace VDFramework.UnityExtensions
+{
+	public static class GameObjectExtensions
+	{
+		public static void DestroyOnLoad(this GameObject gameObject)
+		{
+			SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetActiveScene());
+		}
+		
+		/// <summary>
+		/// Ensures that the specified <see cref="TComponent"/> is on this <see cref="GameObject"/>.
+		/// </summary>
+		public static TComponent EnsureComponent<TComponent>(this GameObject gameObject)
+			where TComponent : Component
+		{
+			return gameObject.TryGetComponent(out TComponent component)
+				? component
+				: gameObject.AddComponent<TComponent>();
+		}
+
+		public static TComponent EnsureComponent<TComponent>(this Component monoBehaviour)
+			where TComponent : Component
+		{
+			return monoBehaviour.gameObject.EnsureComponent<TComponent>();
+		}
+	}
+}
